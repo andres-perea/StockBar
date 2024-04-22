57d576e8f77724b45aba08d0336e7ae24eb6d441
Andres Sanchez <127887122+AndresS2403@users.noreply.github.com>
2024-04-22 10:02:47 -0500

    mensajes flotantes

--- client/src/pages/Menu.jsx
+++ client/src/pages/Menu.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { MdOutlineSearch, MdShoppingCart, MdClose } from "react-icons/md";
+import { MdOutlineSearch, MdShoppingCart, MdClose, MdCheck } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Menu() {
   const [bebidas, setBebidas] = useState([]);
@@ -96,11 +98,13 @@ function Menu() {
       .post("http://localhost:3000/pedidos", pedido)
       .then((response) => {
         console.log("Pedido realizado con éxito:", response.data);
+        toast.success("Pedido realizado con exito");
         setCarrito([]);
         setShowCart(false);
       })
       .catch((error) => {
         console.error("Error al realizar el pedido:", error);
+
       });
   };
 
@@ -123,11 +127,11 @@ function Menu() {
             {/* Botón de búsqueda y carrito */}
             <div className="flex space-x-4">
               <div className="relative">
-              <div className="relative">
-                <input type="text" placeholder="Buscar" className="pl-10 pr-4 py-2 border rounded-lg w-full font-bold" />
-                <MdOutlineSearch className="h-7 w-7 absolute top-1/2 left-3 transform -translate-y-1/2 text-gray-800 pointer-events-none justify-center" />
+                <div className="relative">
+                  <input type="text" placeholder="Buscar" className="pl-10 pr-4 py-2 border rounded-lg w-full font-bold" />
+                  <MdOutlineSearch className="h-7 w-7 absolute top-1/2 left-3 transform -translate-y-1/2 text-gray-800 pointer-events-none justify-center" />
+                </div>
               </div>
-            </div>
 
               <button
                 onClick={toggleCarrito}
@@ -136,34 +140,37 @@ function Menu() {
                 <MdShoppingCart className="h-7 w-7" />
               </button>
               {showCartNotification && (
-                <div className="fixed bottom-10 right-10 bg-gray-900 text-white px-4 py-2 rounded">
+                <div className="fixed bottom-10 right-10 bg-gray-100 text-black px-4 py-2 rounded ">
+                    <MdCheck className="text-green-500" />
                   Producto agregado al carrito
                 </div>
               )}
             </div>
           </div>
         </header>
-        <div className="col-span-1">
+        <div className="flex grid grid-cols-1 lg:grid-cols-6 min-h-screen overflow-hidden ">
+          <div className="col-span-1">
             <div className="flex grid flex flex-col h-full">
               <ul className="flex-1 overflow-y-auto ">
-                
+
                 {categorias.map((categoria, index) => (
-                  <li className="p-2 text-center cursor-pointer odd:bg-white even:bg-slate-50 font-bold table-auto p-6 pb-4 transition duration-500 ease-in-out justify-center">
+                  <li className=" p-2 mt-2 text-center cursor-pointer odd:bg-white even:bg-slate-50 font-bold table-auto p-6 pb-4 transition duration-500 ease-in-out justify-center ">
                     {categoria.nombre}
                     <div className="border-b border-gray-300"></div>
                   </li>
                 ))}
               </ul>
             </div>
+          </div>
           <div className="col-span-5">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
               {bebidas.map((bebida) => (
                 <div
                   key={bebida.codigo}
                   className="max-w-sm rounded overflow-hidden shadow-lg m-1"
-                  >
-                    {/* Imagen producto */}
-                    <img
+                >
+                  {/* Imagen producto */}
+                  <img
                     className="w-full"
                     src="https://via.placeholder.com/350x150"
                     alt="Placeholder"
@@ -196,70 +203,70 @@ function Menu() {
                   </div>
                 </div>
               ))}
-              </div>
             </div>
           </div>
-        </div>
-        {/* Contenedor flotante del carrito */}
-        {showCart && (
-          <div className="fixed right-0 top-0 bg-white shadow-lg w-80 mt-16 p-4">
-            <div className="flex justify-between items-center">
-              <h2 className="text-lg font-bold">Carrito de Compras</h2>
-              <button
-                onClick={toggleCarrito}
-                className="text-red-600 hover:text-red-800"
+        </div></div>
+      {/* Contenedor flotante del carrito */}
+      {showCart && (
+        <div className="fixed right-0 top-0 bg-white shadow-lg w-80 mt-16 p-4">
+          <div className="flex justify-between items-center">
+            <h2 className="text-lg font-bold">Carrito de Compras</h2>
+            <button
+              onClick={toggleCarrito}
+              className="text-red-600 hover:text-red-800"
+            >
+              <MdClose />
+            </button>
+          </div>
+          <div className="mt-4">
+            {carrito.map((item, index) => (
+              <div
+                key={index}
+                className="grid grid-cols-3 gap-4 max-w-sm bg-white rounded overflow-hidden shadow-lg p-2 m-2"
               >
-                <MdClose />
+                <div className="col-span-2 p-2">
+                  <p className="font-bold">{item.nombre}</p>
+                  <p>${item.precio}</p>
+                </div>
+                <div className="flex items-center mt-2">
+                  <button
+                    onClick={() => agregarAlCarrito(item)}
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
+                  >
+                    +
+                  </button>
+                  <span className="mx-2">{item.cantidad}</span>
+                  <button
+                    onClick={() => eliminarDelCarrito(item)}
+                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
+                  >
+                    -
+                  </button>
+                </div>
+              </div>
+            ))}
+            <div className="border-b border-gray-900"></div>
+            <div className="max-w-sm bg-white rounded overflow-hidden shadow-lg p-2 m-2">
+              <p className="font-bold">Total a Pagar: ${total}</p>
+            </div>
+            <div className="flex justify-center">
+              <button
+                onClick={realizarPedido}
+                className="hover:bg-green-700 hover:scale-110 transition duration-400 bg-green-600 text-white font-bold p-2 m-2 mt-2"
+              >
+                Realizar Pedido
+              </button>
+              <button
+                onClick={checkout}
+                className="hover:bg-green-700 hover:scale-110 transition duration-400 bg-green-600 text-white font-bold p-2 m-2 mt-2"
+              >
+                Ver Carrito
               </button>
             </div>
-            <div className="mt-4">
-              {carrito.map((item, index) => (
-                <div
-                  key={index}
-                  className="grid grid-cols-3 gap-4 max-w-sm bg-white rounded overflow-hidden shadow-lg p-2 m-2"
-                >
-                  <div className="col-span-2 p-2">
-                    <p className="font-bold">{item.nombre}</p>
-                    <p>${item.precio}</p>
-                  </div>
-                  <div className="flex items-center mt-2">
-                    <button
-                      onClick={() => agregarAlCarrito(item)}
-                      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
-                    >
-                      +
-                    </button>
-                    <span className="mx-2">{item.cantidad}</span>
-                    <button
-                      onClick={() => eliminarDelCarrito(item)}
-                      className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
-                    >
-                      -
-                    </button>
-                  </div>
-                </div>
-              ))}
-              <div className="border-b border-gray-900"></div>
-              <div className="max-w-sm bg-white rounded overflow-hidden shadow-lg p-2 m-2">
-                <p className="font-bold">Total a Pagar: ${total}</p>
-              </div>
-              <div className="flex justify-center">
-                <button
-                  onClick={realizarPedido}
-                  className="hover:bg-green-700 hover:scale-110 transition duration-400 bg-green-600 text-white font-bold p-2 m-2 mt-2"
-                >
-                  Realizar Pedido
-                </button>
-                <button
-                  onClick={checkout}
-                  className="hover:bg-green-700 hover:scale-110 transition duration-400 bg-green-600 text-white font-bold p-2 m-2 mt-2"
-                >
-                  Ver Carrito
-                </button>
-              </div>
-            </div>
           </div>
-        )}
+          <ToastContainer />
+        </div>
+      )}
     </>
   );
 }
