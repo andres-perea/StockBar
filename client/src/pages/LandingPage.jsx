4c8103289840d3d8eb85f1a3ff4c83aa15d20c99
Andres Sanchez <127887122+AndresS2403@users.noreply.github.com>
2024-05-06 11:34:44 -0500

    Landing

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -9,7 +9,6 @@ import {
   FaInstagram,
   FaTelegram,
   FaCheckCircle,
-  FaPlay,
 } from "react-icons/fa";
 
 function LandingPage() {
@@ -84,6 +83,7 @@ function LandingPage() {
       {/* Section hero */}
 
       {/* End section Hero */}
+
       {/* Main */}
       {/* About Us Section */}
       <section id="about" className="bg-gray-100 py-16">
@@ -97,25 +97,27 @@ function LandingPage() {
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
             <div className="col-span-1 lg:col-span-1">
               <h3 className="text-3xl font-semibold mb-4">UNA PLATAFORMA QUE SE ADAPTA A LAS NECESIDADES DEL CLIENTE</h3>
-              <img src="./src/img/about.jpg" className="rounded-4 mb-4" alt=""  />
+              <img src="./src/img/about.jpg" className="rounded-lg mb-4img-fluid" alt=""  />
               <p className="mb-4">Reconocemos la diversidad de las empresas y sus procesos. ZonaBAR se adapta a diferentes sectores y tamaños de negocio, ofreciendo opciones de personalización para satisfacer las necesidades específicas de cada cliente. Creemos que la verdadera eficiencia proviene de la adaptabilidad, y nuestro software está diseñado para crecer y evolucionar con su empresa.</p>
               <p className="mb-4">Lo que distingue a ZonaBAR es su compromiso con la innovación tecnológica. La empresa emplea tecnologías de vanguardia como inteligencia artificial, aprendizaje automático y análisis predictivo para proporcionar a sus clientes insights profundos sobre sus inventarios. El sistema inteligente de ZonaBAR no solo rastrea la cantidad de existencias, sino que también analiza patrones de demanda, prevé tendencias del mercado y sugiere estrategias de reabastecimiento, todo en tiempo real.</p>
             </div>
             <div className="col-span-1 lg:col-span-1">
               <div className="ps-0 lg:ps-5">
                 <p className="italic mb-4">Con su enfoque innovador y tecnología de vanguardia, ZonaBAR está allanando el camino hacia un futuro donde la gestión de inventarios se convierte en un activo estratégico clave para el éxito empresarial.</p>
-                <ul>
-                  <li><i className=""><FaCheckCircle /></i>Implementar cambios basados en la retroalimentación del usuario para optimizar la experiencia del usuario y la eficiencia operativa.</li>
-                  <li ><i className=""><FaCheckCircle /></i>Implementar funciones de análisis predictivo en tiempo real para permitir a los usuarios tomar decisiones rápidas y fundamentadas.</li>
-                  <li ><i className=""><FaCheckCircle /></i>Fortalecer las medidas de seguridad de datos para garantizar la confidencialidad y la integridad de la información almacenada en la plataforma.</li>
-                  <li ><i className=""><FaCheckCircle /></i>Desarrollar herramientas de analítica avanzada que permitan a las empresas evaluar el rendimiento de su inventario en comparación con los indicadores clave de desempeño (KPI).</li>
-                  <li><i className=""><FaCheckCircle /></i> Investigar y desarrollar herramientas que ayuden a las empresas a evaluar y mejorar la sostenibilidad de sus prácticas de gestión de inventarios.</li>
-                </ul>
+                <ul className="list-inside">
+                <li className="flex items-center mb-4"><FaCheckCircle className="mr-3 text-2xl text-gray-600" />Implementar cambios basados en la retroalimentación del usuario para optimizar la experiencia del usuario y la eficiencia operativa.</li>
+                <li className="flex items-center mb-4"><FaCheckCircle className="mr-3 text-2xl text-gray-600" />Implementar funciones de análisis predictivo en tiempo real para permitir a los usuarios tomar decisiones rápidas y fundamentadas.</li>
+                <li className="flex items-center mb-4"><FaCheckCircle className="mr-3 text-2xl text-gray-600" />Fortalecer las medidas de seguridad de datos para garantizar la confidencialidad y la integridad de la información almacenada en la plataforma.</li>
+                <li className="flex items-center mb-4"><FaCheckCircle className="mr-3 text-2xl text-gray-600" />Desarrollar herramientas de analítica avanzada que permitan a las empresas evaluar el rendimiento de su inventario en comparación con los indicadores clave de desempeño (KPI).</li>
+                <li className="flex items-center mb-4"><FaCheckCircle className="mr-3 text-2xl text-gray-600" />Investigar y desarrollar herramientas que ayuden a las empresas a evaluar y mejorar la sostenibilidad de sus prácticas de gestión de inventarios.</li>
+              </ul>
                 <p className="mb-4">Nuestra plataforma no solo simplifica la gestión de inventarios, sino que la potencia con inteligencia y visión estratégica. Al elegir ZonaBAR, las empresas eligen la innovación y la eficiencia en cada nivel de su cadena de suministro.</p>
 
                 <div className="relative">
-                  <img src="./src/img/about-2.jpg" className="rounded-4" alt="" />
-                  <a href="https://www.youtube.com/watch?v=n1oUspMuUgk" className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></a>
+                  <img src="./src/img/about-2.jpg" className="rounded-lg" alt="" />
+                  <a href="https://www.youtube.com/watch?v=n1oUspMuUgk" className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"> 
+                    <svg className="w-16 h-16 text-white bg-gray-500 rounded-full p-4 transition duration-300 hover:bg-gray-600" fill="currentColor" viewBox="0 0 20 20"><path fillRule="evenodd" d="M8 5v10l7-5z" clipRule="evenodd"/></svg>
+                   </a>
                 </div>
               </div>
             </div>
@@ -126,6 +128,55 @@ function LandingPage() {
         </div>
         </section>
       {/* End About Us Section */}
+      {/* Clients Section*/}
+      <section id="clients" className="bg-gray-100 py-16">
+        <div className="container mx-auto" data-aos="zoom-out">
+
+          <div className="flex justify-center">
+            <div className="swiper-container">
+              <div className="swiper-wrapper flex items-center">
+                <div className="swiper-slide"><img src="./src/img/about.jpg" className="img-fluid" alt="" /> </div>
+                <div className="swiper-slide"><img src="./src/img/about.jpg" className="img-fluid" alt="" /> </div>
+                <div className="swiper-slide"><img src="./src/img/about.jpg" className="img-fluid" alt="" /> </div>
+                <div className="swiper-slide"><img src="./src/img/about.jpg" className="img-fluid" alt="" /> </div>
+                <div className="swiper-slide"><img src="./src/img/about.jpg" className="img-fluid" alt="" /> </div>
+                <div className="swiper-slide"><img src="./src/img/about.jpg" className="img-fluid" alt="" /> </div>
+                <div className="swiper-slide"><img src="./src/img/about.jpg" className="img-fluid" alt="" /> </div>
+                <div className="swiper-slide"><img src="./src/img/about.jpg" className="img-fluid" alt="" /> </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* End Clients Section */}
+      {/* Counter Section*/}
+        <section id="stats-counter" className="bg-gray-100 py-16">
+          <div className="container mx-auto" data-aos="fade-up">
+
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-center">
+
+              <div className="col-span-1 lg:col-span-1">
+                <img src="" className="img-fluid" alt="" />
+              </div>
+
+              <div className="col-span-1 lg:col-span-1">
+                <div className="grid grid-rows-3 gap-4">
+                  <div className="stats-items flex items-center">
+                    <span data-purecounter-start="0" data-purecounter-end="010" data-purecounter-duration="1" className="font-bold text-2xl text-gray-500"><p className="ml-2"><strong>Clientes felices</strong>Lorem ipsum dolor sit amet consectetur.</p></span>
+                  </div>
+                  <div className="stats-items flex items-center">
+                    <span data-purecounter-start="0" data-purecounter-end="010" data-purecounter-duration="1" className="font-bold text-2xl text-gray-500"><p className="ml-2"><strong>Proyectos</strong>Lorem ipsum dolor sit amet consectetur.</p></span>
+                  </div>
+                  <div className="stats-items flex items-center">
+                    <span data-purecounter-start="0" data-purecounter-end="010" data-purecounter-duration="1" className="font-bold text-2xl text-gray-500"><p><strong>Horas de Soporte</strong>Lorem ipsum dolor sit amet consectetur.</p></span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </section>
+      {/* End Counter Section */}
       {/* End Main */}
     </>
   );
