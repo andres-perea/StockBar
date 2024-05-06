0e507bcda295e760ad93cf2de4f5578218166ebc
andres-perea <andresperea1003@gmail.com>
2024-05-06 08:32:56 -0500

    Comentarios

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../index.css";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import {
   FaEnvelope,
   FaPhone,
@@ -43,30 +43,47 @@ function LandingPage() {
         </div>
       </div>
       {/* End TopBar */}
-      { /* Header */}
+      {/* Header */}
       <div className="bg-zinc-500 py-6">
         <div className="container mx-auto flex justify-between items-center">
-            <Link to="/" className="flex items-center text-white">
-                <h1 className="text-2xl font-bold">Zona<span className=" text-zinc-400">BAR.</span></h1>
-            </Link>
-            <nav className="fle items-center">
-                <ul className="flex space-x-4 text-stone-300">
-                    <li><a href="">Incio</a></li>
-                    <li><a href="">Sobre Nosotros</a></li>
-                    <li><a href="">Servicios</a></li>
-                    <li><a href="">Portafolio</a></li>
-                    <li><a href="">Equipo</a></li>
-                    <li><a href="">Contactanos</a></li>
-                    <li><Link to="/login">Iniciar Sesion</Link></li>
-                </ul>
-            </nav>
-
+          <Link to="/" className="flex items-center text-white">
+            <h1 className="text-2xl font-bold">
+              Zona<span className=" text-zinc-400">BAR.</span>
+            </h1>
+          </Link>
+          <nav className="fle items-center">
+            <ul className="flex space-x-4 text-stone-300">
+              <li>
+                <a href="">Incio</a>
+              </li>
+              <li>
+                <a href="">Sobre Nosotros</a>
+              </li>
+              <li>
+                <a href="">Servicios</a>
+              </li>
+              <li>
+                <a href="">Portafolio</a>
+              </li>
+              <li>
+                <a href="">Equipo</a>
+              </li>
+              <li>
+                <a href="">Contactanos</a>
+              </li>
+              <li>
+                <Link to="/login">Iniciar Sesion</Link>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
       {/* End Header */}
-      
-      {/* Section uno */}
+      {/* Section hero */}
 
+      {/* End section Hero */}
+      {/* Main */}
+      {/* End Main */}
     </>
   );
 }
